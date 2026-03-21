31643add7aef64749982e345a01c5f5fdcd7d75d
qminh <133646909+KENTksl@users.noreply.github.com>
2026-03-21 11:56:34 +0700

    fix: fix conflict at tourament controller

--- src/main/java/com/example/football_tourament_web/controller/user/TournamentController.java
+++ src/main/java/com/example/football_tourament_web/controller/user/TournamentController.java
@@ -27,4 +27,34 @@ public class TournamentController {
 	public String competingTeams() {
 		return "user/tournament/Competing_Teams";
 	}
+
+	@GetMapping("/standing-fragment")
+	public String standingFragment() {
+		return "user/tournament/fragments/standing :: standing";
+	}
+
+	@GetMapping("/match-schedule-fragment")
+	public String matchScheduleFragment() {
+		return "user/tournament/Match_schedule :: match-schedule";
+	}
+
+	@GetMapping("/statistics-fragment")
+	public String statisticsFragment() {
+		return "user/tournament/fragments/statistics :: statistics";
+	}
+
+	@GetMapping("/charts-fragment")
+	public String chartsFragment() {
+		return "user/tournament/fragments/charts :: charts";
+	}
+
+	@GetMapping("/bracket-fragment")
+	public String bracketFragment() {
+		return "user/tournament/fragments/bracket :: bracket";
+	}
+
+	@GetMapping("/teams-fragment")
+	public String teamsFragment() {
+		return "user/tournament/fragments/teams :: teams";
+	}
 }
