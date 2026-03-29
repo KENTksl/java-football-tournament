f389d7a30152465605922275907cd0dd214d2679
qminh <133646909+KENTksl@users.noreply.github.com>
2026-03-29 21:45:44 +0700

    Revert "feat: add message for register page"
    This reverts commit 2f34a04b8a88f1caaf22ab9168f9937843189190.

--- src/main/java/com/example/football_tourament_web/repository/AppUserRepository.java
+++ src/main/java/com/example/football_tourament_web/repository/AppUserRepository.java
@@ -1,13 +1,15 @@
 package com.example.football_tourament_web.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.football_tourament_web.model.entity.AppUser;
+import com.example.football_tourament_web.model.enums.UserRole;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 	Optional<AppUser> findByEmail(String email);
-	Optional<AppUser> findByPhone(String phone);
+	List<AppUser> findByRoleOrderByCreatedAtDesc(UserRole role);
 }
 
